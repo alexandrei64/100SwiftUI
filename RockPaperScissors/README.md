bf2a8fa050c9e800d08a598af9728649fc81b012
Alex Andrei <alex_andrei@me.com>
2021-04-07 00:25:58 +0300

    Updates to RockPaperScissors project and related README.md file

--- RockPaperScissors/README.md
+++ RockPaperScissors/README.md
@@ -6,6 +6,12 @@
 ## 🏆 Challenges
 
 > Challenge is to make a brain training game that challenges players to win or lose at rock, paper, scissors.
+> * Each turn of the game the app will randomly pick either rock, paper, or scissors.
+> * Each turn the app will either prompt the player to win or lose.
+> * The player must then tap the correct move to win or lose the game.
+> * If they are correct they score a point; otherwise they lose a point.
+> * The game ends after 10 questions, at which point their score is shown.
+> So, if the app chose “Rock” and “Win” the player would need to choose “Paper”, but if the app chose “Rock” and “Lose” the player would need to choose “Scissors”.
 
 ## 📸 Screenshots
 <!---
--- RockPaperScissors/RockPaperScissors/ContentView.swift
+++ RockPaperScissors/RockPaperScissors/ContentView.swift
@@ -8,11 +8,89 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var choices = ["✊", "✋", "✌️"]
+    @State private var currentChoice = Int.random(in: 0 ... 2)
+    @State private var roundPrompt = Bool.random()
+    @State private var round = 0
+    @State private var score = 0
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color(red: 232, green: 224, blue: 204, opacity: 100).edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            VStack {
+                Text("Score \(score)")
+                    .font(Font.custom("SF Pro Rounded Heavy", size: 25))
+                    .foregroundColor(.black)
+                Spacer()
+                    .frame(height: 50)
+
+                Text(choices[currentChoice])
+                    .font(.system(size: 90))
+                Spacer()
+                    .frame(height: 30)
+                if roundPrompt == true {
+                Text("WIN")
+                    .font(Font.custom("SF Pro Rounded Heavy", size: 30))
+                    .foregroundColor(.black)
+                } else {
+                Text("LOSE")
+                    .font(Font.custom("SF Pro Rounded Heavy", size: 30))
+                    .foregroundColor(.black)
+                }
+                Spacer()
+                    .frame(height: 50)
+                HStack{
+                    ForEach(0 ..< 3) { number in
+                        Button(action: {
+                            self.buttonTapped(number)
+                        }) {
+                            Spacer()
+                                .frame(width:20)
+                            Text("\(self.choices[number])")
+                                .font(.system(size: 75))
+                            Spacer()
+                                .frame(width:20)
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    func buttonTapped (_ number: Int) {
+        if number != currentChoice && roundPrompt == false {
+            score += 1
+            round += 1
+        } else {
+            round += 1
+        }
+        self.askQuestion()
+    }
+    
+    func askQuestion() {
+        choices.shuffle()
+        currentChoice = Int.random(in: 0 ... 2)
+        roundPrompt = Bool.random()
     }
 }
+
+/*
+struct choiceButton: View {
+    let choice: String
+    
+    var body: some View {
+        Button("\(choice)", action: self.buttonTapped(choice))
+    }
+    
+    func buttonTapped(_ choice: Int) {
+        if choice == currentChoice {
+            
+        } else {
+            
+        }
+    }
+}
+*/
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
